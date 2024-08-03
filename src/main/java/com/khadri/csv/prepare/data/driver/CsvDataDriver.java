0a03ded5eb9a80d94168bf098ca6f8589b936545
thasmiyas39 <158719835+thasmiyas39@users.noreply.github.com>
2024-08-02 15:59:46 -1200

    Khadri 300724 804 am Mobile Data (#11)
    * added mobile details
    
    * updated code
    
    * updated files
    
    ---------
    
    Co-authored-by: thasmiyas39@gmail.com <Thasmiya@gmail.com>

--- src/main/java/com/khadri/csv/prepare/data/driver/CsvDataDriver.java
+++ src/main/java/com/khadri/csv/prepare/data/driver/CsvDataDriver.java
@@ -6,6 +6,8 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 import com.khadri.csv.prepare.data.driver.types.DriverTypes;
+import com.khadri.csv.prepare.data.mobile.processor.MobileDataProcessor;
+import com.khadri.csv.prepare.data.mobile.util.MobileFileUtil;
 import com.khadri.csv.prepare.data.employee.processor.EmployeeDataProcessor;
 import com.khadri.csv.prepare.data.employee.util.EmployeeFileUtil;
 import com.khadri.csv.prepare.data.movie.processor.MovieDataProcessor;
@@ -59,6 +61,27 @@ public class CsvDataDriver {
 				System.out.println("The remaining records were inserted into file. ");
 				tupleFilePW._2.flush();
 				tupleFilePW._2.close();
+			}
+
+			break;
+
+		case 6:
+			MobileDataProcessor mobileProcessor = new MobileDataProcessor(scanner);
+			Tuple2<File, PrintWriter> tupleFilePW6 = MobileFileUtil.mobileCsvFilePrintWriter();
+
+			try {
+				System.out.println(" How many records you want to enter ?  : ");
+				int records = scanner.nextInt();
+
+				for (int i = 1; i <= records; i++) {
+					mobileProcessor.process(tupleFilePW6._2, i);
+				}
+			} catch (Exception e) {
+				System.out.println("Exception Occured " + e.getCause());
+			} finally {
+				System.out.println("The remaining records were inserted into file. ");
+				tupleFilePW6._2.flush();
+				tupleFilePW6._2.close();
 			}
 
 			break;
---
+++ src/main/java/com/khadri/csv/prepare/data/mobile/processor/MobileDataProcessor.java
@@ -0,0 +1,48 @@
+package com.khadri.csv.prepare.data.mobile.processor;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+import com.khadri.csv.prepare.data.processor.Processor;
+
+public class MobileDataProcessor implements Processor {
+
+	private Scanner scanner;
+
+	public MobileDataProcessor(Scanner scanner) {
+		this.scanner = scanner;
+	}
+
+	@Override
+	public void process(PrintWriter pw, int recordNumber) {
+
+		System.out.println("===========> MOBILE " + recordNumber + " DATA READING STARTS ===========>");
+		System.out.println("Enter MOBILE_ID : ");
+		int id = scanner.nextInt();
+
+		System.out.println("Enter MOBILE_NAME : ");
+		String name = scanner.next();
+
+		System.out.println("Enter MOBILE_PRICE : ");
+		int price = scanner.nextInt();
+
+		System.out.println("Processing MOBILE DATA");
+
+		System.out.println("Processing MOBILE DETAILS");
+
+		System.out.println("<=========== MOBILE " + recordNumber + " DATA READING ENDS <===========");
+
+		System.out.println("===========> MOBILE" + recordNumber + " DATA WRITING INTO CSV File STARTS ===========>");
+		System.out.println("Processing  File Wrting ");
+
+		//pw.println();// new empty row
+		String line = id + "," + name + "," + price;
+
+		pw.println(line);
+
+		System.out.println();
+		System.out.println("<=========== MOBILE " + recordNumber + " DATA WRITING INTO CSV File ENDS <===========");
+
+	}
+
+}
